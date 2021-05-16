1410b7bafd7a6609ee24bd41dce4dd19d1b06891
Cíntia Fumi <cintiafumi@gmail.com>
2021-05-15 23:33:25 -0300

    ✨  feat: read file and parse lines

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,14 @@
 defmodule GenReport do
+  def build(file) do
+    "reports/#{file}"
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
 
-  def build do
-    # TO DO
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
   end
 end
